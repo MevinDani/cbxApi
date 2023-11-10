386732c6e9accd38dddaf8a4bb00a66749ba2e0a
MEVINDANI <mevindani14@gmail.com>
2023-11-10 13:41:04 +0530

    added sales modal popup

--- src/components/Sales.js
+++ src/components/Sales.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import './Sales.css'
 import { FaSortDown } from "react-icons/fa";
+import { FaListUl } from "react-icons/fa";
 import { RotatingLines } from 'react-loader-spinner'
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 
 
@@ -12,12 +17,25 @@ const TodaySales = () => {
 
     const [totalSales, setTotalSales] = useState(null)
 
+    const [i1date, setI1Date] = useState(30)
+
+    const [openModal, setOpenModal] = useState(false);
+
+    const handleOpen = () => {
+        setOpenModal(true);
+    };
+
+    const handleClose = () => {
+        setOpenModal(false);
+    };
+
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
     }
 
     useEffect(() => {
-        const url = 'https://cubixweberp.com:164/api/Dashboard/DashBoard?cmpcode=PENDULUM&guid=C39F6BCB-86DE-42F2-95B3-3D4DD4278CDA&mod=TOTAL_SALES&s1=%27%27&s2=%27%27&s3=%27%27&i1=100&i2=0&dt1=1-1-2022&dt2=1-1-2022';
+        setTotalSales(null)
+        const url = `https://cubixweberp.com:164/api/Dashboard/DashBoard?cmpcode=PENDULUM&guid=C39F6BCB-86DE-42F2-95B3-3D4DD4278CDA&mod=TOTAL_SALES&s1=%27%27&s2=%27%27&s3=%27%27&i1=${i1date}&i2=0&dt1=1-1-2022&dt2=1-1-2022`;
 
         fetch(url)
             .then(response => response.json())
@@ -31,9 +49,10 @@ const TodaySales = () => {
                 console.log(roundedTotalSales);
             })
             .catch(error => console.error('Error:', error));
-    }, []);
+    }, [i1date]);
 
     console.log(salesData)
+    console.log(i1date)
 
     return (
         <div className='TSSaleswrapper'>
@@ -41,16 +60,54 @@ const TodaySales = () => {
                 <div className='TSMAINDROP'>
                     <div className='TSDropCont'>
                         <div className='TSDropItems'>
-                            <div className='TSDText'>Last 30 days</div>
-                            <div className='TSDText'><FaSortDown onClick={toggleDropdown} /></div>
+                            <div className='TSDText' onClick={() => setI1Date(30)}>Last 30 days</div>
+                            <div className='TSDText'><FaSortDown className='TSDrop' onClick={toggleDropdown} /></div>
+                            <div className='TSDText'><FaListUl className='TSList' onClick={handleOpen} /></div>
                         </div>
                     </div>
+                    <Modal
+                        open={openModal}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Box sx={{ width: 300, bgcolor: '#8AE8FF', outline: 'none', p: 2, height: '300px', overflowY: 'scroll' }}>
+                            <Typography id="modal-modal-title" variant="h6" component="h2">
+                                Sales Data
+                            </Typography>
+                            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                {
+                                    salesData && salesData.map((i, k) => (
+                                        <div className='salesModelCont' key={k}>
+                                            <div className='salesDeptNo'>
+                                                <div className='salesDeptNoText'>Dept. No</div>
+                                                <div className='salesDeptNoNum'>{i.DeptName}</div>
+                                            </div>
+                                            <div className='salesDeptName'>
+                                                <div className='salesDeptNameText'>Dept. Name</div>
+                                                <div className='salesDeptNameNum'>{i.Deptno}</div>
+                                            </div>
+                                            <div className='salesName'>
+                                                <div className='salesNameText'>Sales</div>
+                                                <div className='salesNameNum'>{i.SALES_AMT}</div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </Typography>
+                            <Button sx={{ color: '#FF6A32' }} onClick={handleClose}>Close Modal</Button>
+                        </Box>
+                    </Modal>
                     {isOpen && (
                         <div className='TSDropDown'>
                             <div className='TSDropDownCont'>
-                                <div className='TSDropDownText'>Last 10 days</div>
-                                <div className='TSDropDownText'>Last 5 days</div>
-                                <div className='TSDropDownText'>Last 2 days</div>
+                                <div className='TSDropDownText' onClick={() => setI1Date(10)}>Last 10 days</div>
+                                <div className='TSDropDownText' onClick={() => setI1Date(7)}>Last 7 days</div>
                             </div>
                         </div>
                     )}
--- src/components/TotalPendindPdc.js
+++ src/components/TotalPendindPdc.js
@@ -1,27 +1,31 @@
 import React, { useEffect, useState } from 'react'
-import './TotalPendingPdc.css'
 import { FaSortDown } from "react-icons/fa";
 import { RotatingLines } from 'react-loader-spinner'
+import './TotalPendingPdc.css'
 
 
 
 const TotalPendingPdc = () => {
+
     const [isOpen, setIsOpen] = useState(false);
 
     const [totalPendingPdc, setTotalPendingPdc] = useState(null)
+
+    const [i1date, setI1Date] = useState(100)
 
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
     }
 
     useEffect(() => {
-        const url = 'https://cubixweberp.com:164/api/Dashboard/DashBoard?cmpcode=PENDULUM&guid=C39F6BCB-86DE-42F2-95B3-3D4DD4278CDA&mod=TOTAL_PDCR&s1=%27test%27&s2=%27test%27&s3=%27test%27&i1=100&i2=0&dt1=1-1-2022&dt2=1-1-2022'
+        setTotalPendingPdc(null)
+        const url = `https://cubixweberp.com:164/api/Dashboard/DashBoard?cmpcode=PENDULUM&guid=C39F6BCB-86DE-42F2-95B3-3D4DD4278CDA&mod=TOTAL_PDCR&s1=%27test%27&s2=%27test%27&s3=%27test%27&i1=${i1date}&i2=0&dt1=1-1-2022&dt2=1-1-2022`
 
         fetch(url)
             .then(response => response.json())
             .then(data => setTotalPendingPdc(data))
             .catch(error => console.error('Error:', error));
-    }, [])
+    }, [i1date])
 
     // console.log(totalIssuedPdc)
 
@@ -31,16 +35,15 @@ const TotalPendingPdc = () => {
                 <div className='CBDMAINDROP'>
                     <div className='CBDDropCont'>
                         <div className='CBDDropItems'>
-                            <div className='CBDDText'>Check by date</div>
-                            <div className='CBDDText'><FaSortDown onClick={toggleDropdown} /></div>
+                            <div className='CBDDText' onClick={() => setI1Date(100)}>Last 100 Days</div>
+                            <div className='CBDDText'><FaSortDown className='CBDrop' onClick={toggleDropdown} /></div>
                         </div>
                     </div>
                     {isOpen && (
                         <div className='CBDDropDown'>
                             <div className='CBDDropDownCont'>
-                                <div className='CBDDropDownText'>Last 10 days</div>
-                                <div className='CBDDropDownText'>Last 5 days</div>
-                                <div className='CBDDropDownText'>Last 2 days</div>
+                                <div className='CBDDropDownText' onClick={() => setI1Date(30)}>Last 30 days</div>
+                                <div className='CBDDropDownText' onClick={() => setI1Date(7)}>Last 7 days</div>
                             </div>
                         </div>
                     )}
